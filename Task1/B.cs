2c29ab67cbf45165d1cf0de26cb90f0f56f19340
MiDgEm <Nesterenko_misha1510@mail.ru>
2021-10-13 10:53:00 +0300

    lab1

---
+++ Task1/B.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class B : A
+    {
+        public object PropertyA { get; set; }
+
+        public B(object a)
+        {
+            PropertyA = a;
+        }
+    }
+}
