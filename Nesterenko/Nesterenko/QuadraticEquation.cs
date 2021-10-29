330bed2a2c5be54de2d8e0041585090cbf7d254e
MiDgEm <Nesterenko_misha1510@mail.ru>
2021-10-29 20:17:08 +0300

    lab 3

--- Nesterenko/Nesterenko/QuadraticEquation.cs
+++ Nesterenko/Nesterenko/QuadraticEquation.cs
@@ -1,9 +1,5 @@
-﻿using Nesterenko;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Nesterenko
 {
@@ -11,19 +7,21 @@ namespace Nesterenko
     {
         protected float GetDisriminant(float a, float b, float c)
         {
-            return (float)Math.Pow(b, 2) - 4 * 2 * c;
+            return (float)Math.Pow(b, 2) - 4 * a * c;
         }
 
-        List<float> arrayResult;
+        private List<float> arrayResult;
 
         public List<float> Solve(float a, float b, float c)
         {
             if (a == 0)
+            {
                 return GetLinearEquation(b, c);
+            }
 
             float D = GetDisriminant(a, b, c);
 
-            if (D < 0)
+            if(D < 0)
             {
                 NesterenkoLog.I().Log("Был вызван метод квадратного уравнения");
                 throw new NesterenkoException("Корней нет!");
@@ -32,7 +30,9 @@ namespace Nesterenko
             NesterenkoLog.I().Log("Был вызван метод квадратного уравнения");
 
             if (D == 0)
-                return arrayResult = new List<float>() { (-(float)Math.Pow(b, 2)) / (2 * a) };
+            {
+                return arrayResult = new List<float>() { (-(float)Math.Pow(b, 2)) / (2 * a)};
+            }
 
             return arrayResult = new List<float>() { (-b + (float)Math.Sqrt(D)) / (2 * a), (-b - (float)Math.Sqrt(D)) / (2 * a) };
         }
--- Nesterenko/Properties/AssemblyInfo.cs
+++ Nesterenko/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("Task1")]
+[assembly: AssemblyTitle("Nesterenko")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Task1")]
+[assembly: AssemblyProduct("Nesterenko")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
-[assembly: Guid("517d49b9-5094-439e-9056-b4b3517a1a7a")]
+[assembly: Guid("f33016d9-9b21-4ee4-92ff-af6a44d79b32")]
 
 // Сведения о версии сборки состоят из следующих четырех значений:
 //
